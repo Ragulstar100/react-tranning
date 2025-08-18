8fe445a6e490b07f72c739377e18404e705344f3
Your Name <your.email@example.com>
2025-08-18 20:09:35 +0530

    update

---
+++ test/src/main.jsx
@@ -0,0 +1,12 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import SplashScreen from './splash.jsx'
+import { BrowserRouter } from 'react-router-dom'
+
+createRoot(document.getElementById('r')).render( 
+
+   <App/>
+   
+  )
